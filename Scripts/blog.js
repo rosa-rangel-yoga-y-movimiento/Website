53e941dae10dbe9b04804ba233245a52b4a1c7f5
Luis Fernández <luiggi@fernandez.org.mx>
2020-02-02 00:23:05 -0600

    read blog

---
+++ Scripts/blog.js
@@ -0,0 +1,77 @@
+var blogArray = [];
+
+window.onload = function() {
+  databaseConnection(function() {
+    loadPosts();
+    loadThumbnail();
+  });
+}
+
+function databaseConnection(callback) {
+  var ref = firebase.database().ref().child('/blogs/');
+  ref.once('value', function(snapshot) {
+    var i = 0;
+    snapshot.forEach(function(post) {
+      var postVal = post.val();
+      blogArray[i] = postVal;
+      i += 1;
+      callback();
+    });
+  });
+}
+
+function loadPosts() {
+  var thumbnail_sec = document.getElementById('blog-thumbnails');
+  for (var i = 0; i < blogArray.length; i++) {
+    var anchor = document.createElement("a");
+    anchor.href = "#" + blogArray[i].id;
+    var divi = document.createElement("div");
+    divi.className = "thumbnail";
+    var thumbnail = document.createElement("img");
+    thumbnail.src = blogArray[i].thumbnail;
+    divi.appendChild(thumbnail);
+    var title = document.createElement("h2");
+    title.innerHTML = blogArray[i].titulo;
+    divi.appendChild(title);
+    var subtitle = document.createElement("h3");
+    subtitle.innerHTML = blogArray[i].subtitulo;
+    divi.appendChild(subtitle);
+    anchor.appendChild(divi);
+    thumbnail_sec.appendChild(anchor);
+  }
+}
+
+function loadThumbnail() {
+  var modal_sec = document.getElementById('blog-modals');
+  for (var i = 0; i < blogArray.length; i++) {
+    var divi = document.createElement("div");
+    divi.className = "modal-blog";
+    divi.id = blogArray[i].id;
+    var anchor = document.createElement("a");
+    anchor.title = "Close";
+    anchor.className = "close";
+    anchor.innerHTML = "X";
+    anchor.href = "";
+    divi.appendChild(anchor);
+    var pdf = document.createElement("object");
+    pdf.type = "application/pdf";
+    pdf.data = blogArray[i].blog + "#toolbar=0&navpanes=0&scrollbar=0&messages=0&statusbar=0&View=FitBH";
+    pdf.width = "50%";
+    pdf.height = "100%";
+    pdf.className = "pdf";
+    divi.appendChild(pdf);
+    modal_sec.appendChild(divi);
+    var url = window.location.href.split("#");
+    var last = url.pop();
+    if (last == blogArray[i].id) {
+      console.log("xx");
+      var selectedPost = document.getElementById(blogArray[i].id);
+      selectedPost.className += " selected"
+      selectedPost.opacity = "1";
+      selectedPost.zIndex = "99999";
+      selectedPost.pointerEvents = "auto";
+      selectedPost.cursor = "default";
+      console.log(selectedPost);
+    }
+  }
+}
